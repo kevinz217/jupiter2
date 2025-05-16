064ea988c59e03436f677d4ddbf9c52c93500629
BT_4N2_09 <sample@bths.edu>
2025-05-16 12:48:54 -0400

    it starts

---
+++ Main.py
@@ -0,0 +1,53 @@
+import mysql.connector
+import getpass
+def createConnection(username, password):
+   connection = mysql.connector.connect(user=username,
+                                        password=password,
+                                        host='10.8.37.226',
+                                        database=username + "_db")
+   return connection
+
+
+def executeStatement(connection, query):
+   cursor = connection.cursor()
+   cursor.execute(query)
+   studentList = []
+
+
+   for row in cursor:
+       studentList.append(row)
+
+
+   cursor.close()
+   connection.close()
+   return studentList
+
+
+
+
+def getStudentSchedule(student_ID):
+   statement = "CALL getStudentClasses(" + student_ID + ")"
+   return executeStatement(createConnection(username, password), statement)
+
+
+
+
+# anti owen code
+username = getpass.getpass("Enter your username: ")
+password = getpass.getpass("What is your password: ")
+student_ID = input("Which student do you want? ")
+
+
+studentInfo = getStudentSchedule(student_ID)
+
+
+for info in studentInfo:
+   teacherName = info[4]
+   roomNumber = info[3]
+   courseName = info[2]
+   period = info[1]
+   print("Period: " + str(period))
+   print("Course: " + courseName)
+   print("Room Number: " + roomNumber)
+   print("Teacher: " + teacherName)
+   print()

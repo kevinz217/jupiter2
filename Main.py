d2d2ab1a927ab8e994d7e4e144414cec4709e647
BT_4N2_09 <sample@bths.edu>
2025-05-16 13:16:30 -0400

    started teacher version

--- Main.py
+++ Main.py
@@ -29,25 +29,37 @@ def getStudentSchedule(student_ID):
    statement = "CALL getStudentClasses(" + student_ID + ")"
    return executeStatement(createConnection(username, password), statement)
 
-
+def getTeacherSchedule(teacher_ID):
+   return
 
 
 # anti owen code
 username = getpass.getpass("Enter your username: ")
 password = getpass.getpass("What is your password: ")
-student_ID = input("Which student do you want? ")
+answer = input("Are you a student or a teacher? ")
+answer = answer.lower()
+if answer == "student" | answer == "s":
+   student_ID = input("Enter your studentID: ")
+   studentInfo = getStudentSchedule(student_ID)
+   for info in studentInfo:
+      teacherName = info[4]
+      roomNumber = info[3]
+      courseName = info[2]
+      period = info[1]
+      print("Period: " + str(period))
+      print("Course: " + courseName)
+      print("Room Number: " + roomNumber)
+      print("Teacher: " + teacherName)
+      print()
 
-
-studentInfo = getStudentSchedule(student_ID)
-
-
-for info in studentInfo:
-   teacherName = info[4]
-   roomNumber = info[3]
-   courseName = info[2]
-   period = info[1]
-   print("Period: " + str(period))
-   print("Course: " + courseName)
-   print("Room Number: " + roomNumber)
-   print("Teacher: " + teacherName)
-   print()
+elif answer == "teacher" | answer == "t":
+   teacher_ID = input("Enter your studentID: ")
+   teacherInfo = getTeacherSchedule(teacher_ID)
+   for info in teacherInfo:
+      roomNumber = info[3]
+      courseName = info[2]
+      period = info[1]
+      print("Period: " + str(period))
+      print("Course: " + courseName)
+      print("Room Number: " + roomNumber)
+      print()
